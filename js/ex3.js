58a7c834ece221f8788eddc7a8e13cfdb7b0bc7e
IrisWu <xianwu_iris@126.com>
2021-04-03 17:11:37 +0800

    change

--- js/ex3.js
+++ js/ex3.js
@@ -2,3 +2,33 @@
 Exercise 3 JavaScript code
 */
 console.log("Wu's Output from Homework 8 Exercise 3");
+
+const travelInformation = {
+        name: "Iris",
+        countries: [
+            {
+                name: "UK",
+                year: 2017
+            },
+            {
+                name: "USA",
+                year: 2019
+            }
+        ]
+};
+
+fetch(" https://thejsway-server.herokuapp.com/api/countries", {
+    method: "POST",
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(travelInformation)
+})
+  .then(response => response.text())
+  .then(result => {
+      console.log(result);
+  })
+  .catch(err =>{
+      console.error(err.message);
+  });
--- js/ex4.js
+++ js/ex4.js
@@ -2,3 +2,32 @@
 Exercise 4 JavaScript code
 */
 console.log("Wu's Output from Homework 8 Exercise 4");
+
+
+document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const resultElement = document.getElementById("result");
+    const tableElement = document.createElement("table");
+    tableElement.classList = "user";
+    const trElement1 = document.createElement("tr");
+    trElement1.innerHTML = "<th> Key </th><th> Value </th>"
+    resultElement.appendChild(tableElement).appendChild(trElement1);
+
+    for (let pair of formData.entries()) {
+        console.log(pair[0] + ',' + pair[1]);
+        const trElement = document.createElement("tr");
+        const tdElement1 = document.createElement("td");
+        const tdElement2 = document.createElement("td");
+        tdElement1.textContent = pair[0];
+        tdElement2.textContent = pair[1];
+        trElement.appendChild(tdElement1);
+        trElement.appendChild(tdElement2);
+        tableElement.appendChild(trElement);
+
+    }
+
+    
+})
+
+
